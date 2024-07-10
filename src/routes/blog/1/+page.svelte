a00a7c46a476ddb0690a556ab902d5a61a826376
Ryan Atkinson <mail@ryanatkn.com>
2024-07-10 10:09:49 -0600

    wip

---
+++ src/routes/blog/1/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+	import type {Blog_Post_Data} from '$lib/blog.js';
+
+	export const post = {
+		title: 'Hello fuz_blog',
+		slug: 'hello-fuz-blog',
+		date_published: '2024-07-10T16:04:49.688Z',
+		date_modified: '2024-07-10T16:04:49.688Z',
+		summary: 'fuz_blog is now a reusable library',
+		tags: ['blog software', 'sveltekit'],
+	} satisfies Blog_Post_Data;
+</script>
+
+<script lang="ts">
+	import {base} from '$app/paths';
+</script>
+
+<p>
+	<a href="{base}/a/b/c">todo a local link example</a>
+</p>
