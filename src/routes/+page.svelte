b010e91202533cb2fa72170e669479abf6ddb14b
Ryan Atkinson <mail@ryanatkn.com>
2024-07-10 09:16:23 -0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Card from '@ryanatkn/fuz/Card.svelte';
+	import {base} from '$app/paths';
+	import {random_item} from '@ryanatkn/belt/random.js';
+
+	import Mreows, {items} from '$routes/Mreows.svelte';
+
+	let mreows: Array<{glyph: string}> | undefined = $state([random_item(items), items[4]]);
+</script>
+
+<main>
+	<section class="box">
+		<header>
+			<h1 class="mt_xl2">fuz_template</h1>
+		</header>
+		<Card href="{base}/about" icon={mreows ? mreows[0].glyph : '✨'}>about</Card>
+	</section>
+	<Mreows bind:mreows />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Package_Detail from '@ryanatkn/fuz/Package_Detail.svelte';
+	import {parse_package_meta} from '@ryanatkn/gro/package_meta.js';
+	import Library_Footer from '@ryanatkn/fuz/Library_Footer.svelte';
+	import Community_Links_Panel from '@ryanatkn/fuz/Community_Links_Panel.svelte';
+	import Breadcrumb from '@ryanatkn/fuz/Breadcrumb.svelte';
+
+	import {package_json, src_json} from '$routes/package.js';
+
+	const pkg = parse_package_meta(package_json.homepage, package_json, src_json);
+
+	// TODO standardize
+</script>
+
+<main class="width_md">
+	<section class="box">
+		<header class="box">
+			<h1 class="mt_xl2">fuz_template</h1>
+		</header>
+		<div style:--size="var(--size_xl)">
+			<Breadcrumb>{package_json.glyph}</Breadcrumb>
+		</div>
+	</section>
+	<Community_Links_Panel />
+	<section class="box w_100 mb_lg">
+		<div class="panel p_lg width_md">
+			<Package_Detail {pkg} />
+		</div>
+	</section>
+	<section class="box">
+		<Library_Footer {pkg} root_url="https://www.fuz.dev/" />
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 0 auto;
+	}
+	h1 {
+		display: flex;
+		align-items: baseline;
+		text-align: center;
+	}
+	section {
+		margin-bottom: var(--space_xl5);
+	}
+</style>
